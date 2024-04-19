a7efe7298adbebde8106c0f3441449dadca9855e
(╯✧∇✧)╯KamKam <kamillayla@gmail.com>
2024-04-19 15:04:14 -0400

    Object Persistence
    So objects that are carried over from scene to scene will remain the same

---
+++ Color Puzzle Dev Kit/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    //array has to be big enough to store all the objects that you want to stay the same across different scenes
+    private static GameObject[] persistentObjs = new GameObject[10]; 
+    public int objIndex;
+
+    void Awake()
+    {
+        //if this array is empty add the current object
+        if(persistentObjs[objIndex] == null)
+        {
+            persistentObjs[objIndex] = gameObject;
+            //don't destroy when the scene loads
+            DontDestroyOnLoad(gameObject);
+        }
+
+        //if there is a duplicate game object in the array, delete it
+        else if (persistentObjs[objIndex] != gameObject)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
